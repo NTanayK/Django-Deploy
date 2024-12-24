7be8d504ca62da48c698b30a57fd3d3e888ed32e
Tanay Kumar <tanaykumar251@gmail.com>
2024-12-24 13:52:03 +0530

    Host

--- PortalProject/settings.py
+++ PortalProject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-!f+85b52#7g0uz&3n$=qcrhie-4^7y4*fwb&_5p=w$4-#ucrjj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
